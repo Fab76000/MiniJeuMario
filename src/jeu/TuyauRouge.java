67decf87bf14c22fe17899ea1649df48d36f2804
Fabienne Bergès <fabienne.berges@univ-rouen.fr>
2023-02-26 18:09:51 +0100

    Mini jeu Mario

---
+++ src/jeu/TuyauRouge.java
@@ -0,0 +1,17 @@
+package jeu;
+
+import javax.swing.ImageIcon;
+
+public class TuyauRouge extends Objet{
+    
+    //**** VARIABLES ****//
+    
+    
+    //**** CONSTRUCTEUR ****//  
+    public TuyauRouge(int x, int y) {
+        super(x, y, 43, 65);        
+        super.icoObjet = new ImageIcon(getClass().getResource("/images/tuyauRouge.png"));
+        super.imgObjet = this.icoObjet.getImage();
+    }
+    
+}
